e164f5468c0bd1df461145e718d6cacfdc620f32
daviddcho <amazingdcho@gmail.com>
2020-11-07 23:22:14 -0500

    removed redundent exit, and comment stuff

--- myshell.cpp
+++ myshell.cpp
@@ -5,44 +5,44 @@
 #include <fstream> // for reading file
 using namespace std;
 
+
+string get_cur_path();
+
+
+int main(int argc, char *argv[], char *envp[]) {
+
+  string line;
+  if (argc > 1) {
+    // Get the batch file
+    string filename = argv[1]; 
+    ifstream file(filename); 
+
+    if (file.is_open()) {
+      while(getline(file, line)) {
+        // Parse the line
+        parseLine(line);
+      }
+      // Close the file
+      file.close();
+      exit(0);
+    }
+  // If there is no batch file
+  } else {
+    while(true) {
+      cout << "myshell ~" << get_cur_path()  << "$ ";
+      // Get user input 
+      getline(cin, line);
+      // Parse user input
+      parseLine(line); 
+    }
+  }
+  return 0;
+}
+
+
+// Returns current path
 string get_cur_path() {
   char temp[PATH_MAX];
   return (getcwd(temp, sizeof(temp)) ? string(temp) : string(""));
 }
 
-// myshell 
-int main(int argc, char *argv[], char *envp[]) {
-
-  string line;
-  
-  if (argc > 1) {
-    // get the batch file
-    string filename = argv[1]; 
-    ifstream file(filename); 
-    // if the file is open
-    if (file.is_open()) {
-      // while getting line
-      while(getline(file, line)) {
-        // parse the line
-        parseLine(line);
-      }
-      // close the file
-      file.close();
-      // exit program
-      exit(0);
-    }
-  // if there is no batch file
-  } else {
-    while(true) {
-      cout << "myshell ~" << get_cur_path()  << "$ ";
-      // get user input 
-      getline(cin, line);
-      // parse user input
-      parseLine(line); 
-      if ((line == "exit") && (!cin.eof())) {
-        exit(0);
-      }
-    }
-  }
-  return 0;
-}
