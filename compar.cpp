27d489cb6073261d0f2ccee936c0df0ff651ea50
daviddcho <amazingdcho@gmail.com>
2020-11-08 23:39:19 -0500

    little cleaning up

--- compar.cpp
+++ compar.cpp
@@ -10,13 +10,13 @@ void parseLine(string line) {
   char command[line.size() + 1];
   strcpy(command, line.c_str());
   
+  // Handler for when user enters nothing
   if (command[0] == '\0') {
     return;
   }
   // Tokenizing the input by spaces
   // ex. {"ls", "-l", "|", "wc"}
   char* command_token = strtok(command, " ");
-
   
   // Organizing command arguments as groups (vector of vector pointers) 
   vector<vector<char*> * > parsedGroups;
@@ -25,12 +25,10 @@ void parseLine(string line) {
   parsedGroups.push_back(new vector<char*>);
 
   int i = 0; // iterator for parsedGroup
-
   while (command_token != NULL) {
     if (isShellCommand(command_token)) {
       // Add NULL to the current group (not shell group);
       parsedGroups.at(i)->push_back(NULL); 
-
       // Create a new group for shell command
       parsedGroups.push_back(new vector<char*>);
       // Iterate to shell command
@@ -39,9 +37,6 @@ void parseLine(string line) {
       parsedGroups.at(i)->push_back(command_token);
       // Add null to shell group
       parsedGroups.at(i)->push_back(NULL);
-
-      // Iterate to next group
-      //i++;
     } else {
       // If this is the first group (there is no shell command), add to current group
       // Or if the last command  
@@ -56,20 +51,17 @@ void parseLine(string line) {
         parsedGroups.at(i)->push_back(command_token);
       }
     }
-
     // Used to iterate through command token
     command_token = strtok(NULL, " ");
   }
-  
+  // Error check if iteration or group making messed up 
   if (parsedGroups.size() < i) {
     cout << "Error: parsed groups size and iteration don't add up." << '\n';
   }
   // Add NULL to the last group
   parsedGroups.at(i)->push_back(NULL); 
-
   
   processCommand(parsedGroups);
-     
   return;
 }
 
